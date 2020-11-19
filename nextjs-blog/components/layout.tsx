0812c08c7a3ea9d9431d825bd654dd62da2eebc4
Your Name <dhdezllerena94@gmail.com>
2020-11-19 15:25:18 +0000

    feat web title

--- nextjs-blog/components/layout.tsx
+++ nextjs-blog/components/layout.tsx
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Daniel Hernández'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Next.js Sample Website Created by Daniel Hernández'
 
 export default function Layout({
   children,
