6d284dfa87eb04829f7f2b80cbea7737b8e3c009
Your Name <dhdezllerena94@gmail.com>
2020-11-19 12:03:15 +0000

    feat: dynamic routes

---
+++ nextjs-blog/components/post.js
@@ -0,0 +1,11 @@
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
